dbb36f450f19a8c4dbf6332400a522d33221d387
DonHala <halapovaev@yandex.ru>
2024-05-20 20:24:57 +0300

    Добавление стартовой иконки

--- AppForMyDaughter/Presenter/StartViewController.swift
+++ AppForMyDaughter/Presenter/StartViewController.swift
@@ -8,11 +8,29 @@
 import UIKit
 
 class StartViewController: UIViewController {
-    
 
+    let image: UIImageView = {
+        var image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "loaderScreen")
+        image.contentMode = .scaleAspectFill
+        return image
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
+        view.addSubview(image)
+        setupConstraints()
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            image.topAnchor.constraint(equalTo: view.topAnchor),
+            image.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            image.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            image.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
     }
 }
 
