25c4737819c01287c5a93e952414415f1838ac9f
DonHala <halapovaev@yandex.ru>
2024-05-25 21:01:46 +0300

    Исправление ошибок

--- AppForMyDaughter/AppSetting/DIContainer.swift
+++ AppForMyDaughter/AppSetting/DIContainer.swift
@@ -5,13 +5,17 @@
 //  Created by DonHalab on 25.05.2024.
 //
 
+import Foundation
+
 class DIContainer {
     
     static let shared = DIContainer()
     private init() {}
     
     func makeOpenAIService() -> OpenAIServiceProtocol {
-        let apiKey = ""
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "OPENAI_API_KEY") as? String else {
+            fatalError("API Key not found")
+        }
         let systemMessage = "Ты мой ассистент"
         let model: OpenAIChatService.GptModel = .gpt4o
         return OpenAIChatService(model: model, apiKey: apiKey, systemMessage: systemMessage)
--- AppForMyDaughter/AppSetting/SceneDelegate.swift
+++ AppForMyDaughter/AppSetting/SceneDelegate.swift
@@ -10,24 +10,25 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var diContainer: DIContainer?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = StartViewController()
+        
+        diContainer = DIContainer.shared
+        
+        let startViewController = StartViewController()
+        
+        if let container = diContainer {
+            startViewController.openAI = container.makeOpenAIService()
+        }
+        
+        window.rootViewController = startViewController
         window.makeKeyAndVisible()
         self.window = window
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {}
-
-    func sceneDidBecomeActive(_ scene: UIScene) {}
-
-    func sceneWillResignActive(_ scene: UIScene) {}
-
-    func sceneWillEnterForeground(_ scene: UIScene) {}
-
-    func sceneDidEnterBackground(_ scene: UIScene) {}
 }
 
